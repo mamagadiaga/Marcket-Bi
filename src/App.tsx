6ca7df8860c9adc0c4bae32e33491f5d43e97597
Mama GDG <mamsogdg@gmail.com>
2024-02-08 16:30:23 +0000

    home

--- src/App.tsx
+++ src/App.tsx
@@ -214,9 +214,51 @@ const App = () => {
 
       </div>
 
+      {/* section 3
 
 
-      {/* section3 */}
+
+      <div id="controls-carousel" className="relative w-full" data-carousel="static">
+
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-2'>
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://api.market-bi.com/media/photo_profil/default_avatar.png" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+              <img src="https://api.market-bi.com/media/photo_profil/315063180_113184091604014_1853243680828194684_n.jpg" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+              <img src="https://api.market-bi.com/media/photo_profil/FB83CE44-8634-4EDF-97B8-5E3E3EFCA3B4.png" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+            </div>
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://api.market-bi.com/media/photo_profil/default_avatar.png" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+              <img src="https://api.market-bi.com/media/photo_profil/315063180_113184091604014_1853243680828194684_n.jpg" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+              <img src="https://api.market-bi.com/media/photo_profil/FB83CE44-8634-4EDF-97B8-5E3E3EFCA3B4.png" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+            </div>
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://api.market-bi.com/media/photo_profil/default_avatar.png" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+              <img src="https://api.market-bi.com/media/photo_profil/315063180_113184091604014_1853243680828194684_n.jpg" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+              <img src="https://api.market-bi.com/media/photo_profil/FB83CE44-8634-4EDF-97B8-5E3E3EFCA3B4.png" className="absolute block -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-28" alt="..." />
+            </div>
+          </div>
+        </div>
+        <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+        <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      </div> */}
+
+
+      {/* section4 */}
       <div className='p-4 mt-10'>
         <p className='text-xl md:text-3xl font-semibold'>Notre solution tout en un</p>
 
@@ -335,10 +377,10 @@ const App = () => {
                     <div className="flex">
                       <input type="text" id="website-admin" className="rounded-none rounded-s-lg bg-gray-50 border focus:border-0 text-gray-900 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5" placeholder="marketbisn@gmail.com" />
                       <span className="bg-teal-500 inline-flex items-center px-3 text-sm text-white border rounded-s-0 rounded-e-md">
-                       <a href="#" type='button'>
-                       <i className="bi bi-caret-right-fill"></i>  
-                       </a>
-                        </span>
+                        <a href="#" type='button'>
+                          <i className="bi bi-caret-right-fill"></i>
+                        </a>
+                      </span>
                     </div>
                   </li>
                   <li>
